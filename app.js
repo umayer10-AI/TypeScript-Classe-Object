4775fb50fb0519ad963cd0115a23c2b208df9af9
umayer10-AI <mdumayer10@gmail.com>
2026-05-23 18:49:06 +0600

    add

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let a = 10;
+const a = (obj) => {
+};
+a({ name: "Umayer", email: "hello", password: "abcd", age: 21 });
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-let a = 10
+// let a = 10
+interface user {
+    name: string,
+    email: string,
+    password: string,
+}
+interface admin extends user {
+    age: number
+}
+
+const a = (obj: admin) => {
+    
+}
+
+a({name: "Umayer", email:"hello", password:"abcd",age:21})
